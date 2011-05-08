88cc7430fce0ea99db265d30520c88f51e5e9d91
Markus Olsson <j.markus.olsson@gmail.com>
2011-05-08 20:05:43 +0200

    Enabling Code Analysis for debug builds

--- freakcode.frequency/DefaultMonotonicTimeProvider.cs
+++ freakcode.frequency/DefaultMonotonicTimeProvider.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace freakcode.frequency
 {
     public class DefaultMonotonicTimeProvider : IMonotonicTimeProvider
     {
+        [SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes", Justification = "DefaultMonotonicTimeProvider is immutable")]
         public static readonly DefaultMonotonicTimeProvider Instance = new DefaultMonotonicTimeProvider();
 
         public long Now
